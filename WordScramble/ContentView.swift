0971ad12bc5690ac1ca9e73188febe7c1d94d63e
Jack Lingle <captainjack0818@gmail.com>
2023-12-05 18:14:16 -0500

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Jack Lingle on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jack Lingle on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
