912aefdccd90b71133adb4e9bd9992fc0563262b
Jack Lingle <captainjack0818@gmail.com>
2023-12-07 19:45:19 -0500

    add basic text field and list of used words

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }.navigationTitle(rootWord)
+                .onSubmit(addNewWord)
         }
-        .padding()
+    }
+    func addNewWord() {
+        newWord = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard newWord.count > 0 else { return }
+        withAnimation {
+            usedWords.insert(newWord, at: 0)
+        }
+        newWord = ""
     }
 }
 
